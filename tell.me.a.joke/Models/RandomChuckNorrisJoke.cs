c3e4b0cb2f35d6113b6eecbcdfff0fe25f74d140
Dilan Livera <Dylan_yahampath@yahoo.com>
2020-08-31 22:55:31 +1000

    add logic to get a joke from chuck norris jokes api

---
+++ tell.me.a.joke/Models/RandomChuckNorrisJoke.cs
@@ -0,0 +1,8 @@
+﻿namespace tell.me.a.joke.Models
+{
+    public class RandomChuckNorrisJoke
+    {
+        public string Type { get; set; }
+        public ChuckNorrisJoke Value { get; set; }
+    }
+}
